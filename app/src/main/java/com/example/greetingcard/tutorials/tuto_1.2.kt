a5d9afb9db7770cf3d845f6508c1e6758c3a530c
Samiaboug <bougtoubsamia@gmail.com>
2024-10-07 14:58:32 +0100

    tutorials

---
+++ app/src/main/java/com/example/greetingcard/tutorials/tuto_1.2.kt
@@ -0,0 +1,10 @@
+package com.example.greetingcard.tutorials
+
+fun main() {
+    val count: Int = 2
+    println(count)
+    val unreadCount = 5
+    val readCount = 100
+    println("You have ${unreadCount + readCount} total messages in your inbox.")
+
+}
