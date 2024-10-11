67314c2b4f3620905b33f2bf56df15fcddfc4822
Samiaboug <bougtoubsamia@gmail.com>
2024-10-11 12:05:21 +0100

    Mini projet

--- ProjetCartedevisite/app/src/main/java/com/example/projet_cartedevisite/MainActivity.kt
+++ ProjetCartedevisite/app/src/main/java/com/example/projet_cartedevisite/MainActivity.kt
@@ -13,10 +13,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Alignment
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 
@@ -73,18 +75,19 @@ fun CarteVisite(name: String,title : String , history : String ,modifier: Modifi
                 Image(
                     painter = image,
                     contentDescription = null ,
+                    modifier = modifier.shadow(9.dp, shape = RoundedCornerShape(8.dp))
                 )
             }
 
             Text(
                 text = name,
                 fontSize = 16.sp,
-                modifier = Modifier.padding(8.dp),
+                modifier = Modifier.padding(top = 18.dp),
                 fontWeight = FontWeight.Bold
             )
             Text(
                 text = title,
-                fontSize = 10.sp,
+                fontSize = 12.sp,
                 fontWeight = FontWeight.SemiBold,
                 modifier = Modifier.padding(8.dp)
 
