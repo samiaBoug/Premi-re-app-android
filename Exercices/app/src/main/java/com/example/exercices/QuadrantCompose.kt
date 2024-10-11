c056ca1dfde2aaa944ce31e3c0a8cdbabb53296c
Samiaboug <bougtoubsamia@gmail.com>
2024-10-11 09:05:46 +0100

    Exercices

--- Exercices/app/src/main/java/com/example/exercices/QuadrantCompose.kt
+++ Exercices/app/src/main/java/com/example/exercices/QuadrantCompose.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,67 +18,91 @@ class QuadrantCompose : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExercicesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    QuadrantCompose(
 
-                    )
-                }
+                QuadrantCompose (
+                    titre1 = "Text composable",
+                    titre2 = "Image composable",
+                    titre3 = "Row composable",
+                    titre4 = "Column composable",
+                    contenue1 = "Displays text and follows the recommended Material Design guidelines." ,
+                    contenue2 = "Creates a composable that lays out and draws a given Painter class object." ,
+                    contenue3 = "A layout composable that places its children in a horizontal sequence." ,
+                    contenue4 = "A layout composable that places its children in a vertical sequence."
+
+                )
+
             }
         }
     }
 }
 
+@Composable
+fun TextCompose(titre : String , text : String , modifier : Modifier){
+    Column {
+        Text(
+            text = titre
+        )
+        Text(
+            text = text
+
+        )
+    }
+}
+@Composable
+fun ImageCompose(titre : String , text : String , modifier : Modifier= Modifier){
+    Column {
+        Text(
+            text = titre
+        )
+        Text(
+            text = text
+
+        )
+    }
+}
+@Composable
+fun RowCompose(titre : String , text : String , modifier : Modifier= Modifier){
+    Column {
+        Text(
+            text = titre
+        )
+        Text(
+            text = text
+
+        )
+    }
+}
+@Composable
+fun ColumnCompose(titre : String , text : String , modifier : Modifier= Modifier){
+    Column {
+        Text(
+            text = titre
+        )
+        Text(
+            text = text
+        )
+    }
+}
 
 @Composable
+fun QuadrantCompose(titre1 : String ,titre2 : String , titre3 : String ,titre4 : String ,contenue1 : String, contenue2 : String,contenue3 : String,contenue4 : String,modifier : Modifier= Modifier){
 
-fun QuadrantCompose(titre1 : String , contenue1 : String , titre2 : String , contenue2 : String , titre3 : String , contenue3 : String , titre4 : String , contenue4 : String , modifier: Modifier = Modifier) {
-
-    Column(modifier = Modifier) {
+    Column {
         Row {
-            Column {
-                Text(
-                    text = titre1
-                )
-                Text(
-                    text = contenue1
-                )
-            }
-
-            Column {
-                Text(
-                    text = titre2
-                )
-                Text(
-                    text = contenue2
-                )
-            }
+            TextCompose(titre = titre1, text = contenue1, modifier = Modifier)
+            ImageCompose(titre = titre2, text = contenue2, modifier = Modifier)
         }
 
-        Row {
-            Column {
-                Text(
-                    text = titre3
-                )
-                Text(
-                    text = contenue3
-                )
-            }
 
-            Column {
-                Text(
-                    text = titre4
-                )
-                Text(
-                    text = contenue4
-                )
-            }
+        Row {
+            RowCompose(titre = titre3, text = contenue3, modifier = Modifier)
+            ColumnCompose(titre = titre4, text = contenue4, modifier = Modifier)
         }
     }
 
+}
 
-  }
-
-@Preview(showBackground = true)
+@Preview(showBackground = true )
 @Composable
 fun QuadrantComposePreview() {
     ExercicesTheme {
@@ -95,6 +115,7 @@ fun QuadrantComposePreview() {
             contenue2 = "Creates a composable that lays out and draws a given Painter class object." ,
             contenue3 = "A layout composable that places its children in a horizontal sequence." ,
             contenue4 = "A layout composable that places its children in a vertical sequence."
+
         )
 
 
